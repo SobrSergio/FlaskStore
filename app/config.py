efe6f7d6d4b0876d3431603952c8e8d860ae578c
SobrSergio <sobr.sergio@gmail.com>
2022-09-05 22:43:02 +0300

    config

--- app/config.py
+++ app/config.py
@@ -4,12 +4,12 @@ class Configuration():
     SECRET_KEY = 'pilpy4gwoeygel32yge2oyureg2ley2gdl2yglyg'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Sergey120799Tamara2607@localhost/sneakshop'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://YourDb'
     
     
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'SneakShop.ru@gmail.com'
-    MAIL_PASSWORD = 'uzowevoofbjfulba'
+    MAIL_PASSWORD = 'passwordforyouremail'
     MAIL_DEFAULT_SENDER = 'SneakShop.ru@gmail.com'
