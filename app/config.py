2a4f77169de5e43fe6aee6eab2a1d252bd0cb743
SobrSergio <sobr.sergio@gmail.com>
2022-09-13 01:31:30 +0300

    mainchange

--- app/config.py
+++ app/config.py
@@ -1,15 +1,15 @@
 class Configuration():
     DEBUG = True
     
-    SECRET_KEY = 'pilpy4gwoeygel32yge2oyureg2ley2gdl2yglyg'
+    SECRET_KEY = 'HERE-YOUR-SECRET-KEY'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://YourDb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://YourDb'  #Example connection on postgresql- 'postgresql://postgres:YourPassword@localhost/sneakshop'
     
     
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.googlemail.com' 
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'youremail@gmail.com'
-    MAIL_PASSWORD = 'passwordforyouremail'
-    MAIL_DEFAULT_SENDER = 'youremail@gmail.com'
+    MAIL_USERNAME = 'youremail@gmail.com' #enter email admin
+    MAIL_PASSWORD = 'passwordforyouremail' #enter password email admin
+    MAIL_DEFAULT_SENDER = 'youremail@gmail.com' #enter email admin
